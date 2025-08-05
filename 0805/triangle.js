67f31e9039cf41ee6c952541667ace2ed62b2d7b
Dylan Tai <dylztai@gmail.com>
2025-08-05 18:01:44 -0400

    Last minute changes

--- 0805/triangle.js
+++
@@ -1,9 +0,0 @@
-// Prints a equal legged right triangle with the size of 7
-
-for (let i = 1; i <= 7; i++){
-    let output = "";
-    for (let j = i; j > 0; j--){
-        output += "#";
-    }
-    console.log(output);
-}
