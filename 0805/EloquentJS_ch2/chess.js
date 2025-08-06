b49bc2d631b9079aaaace1f96f58f47f870e8d3e
Dylan Tai <dylztai@gmail.com>
2025-08-06 11:16:39 -0400

    Added more to chess.js

--- 0805/EloquentJS_ch2/chess.js
+++ 0805/EloquentJS_ch2/chess.js
@@ -1,5 +1,9 @@
 // Creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character.
 
+
+
+/*
+// Nested for loop for changed sizes
 const size = 8;
 let board = "";
 
@@ -14,4 +18,80 @@ for (let i = 0; i < size; i++) {
         board += "\n";
 }
 
+console.log(board);
+*/
+
+
+
+/*
+// Single for loop with changed sizes, only one variable
+const size = 8;
+let board = "";
+
+for (let i = 1; i < size * size + size; i++){
+    if (i % (size + 1) === 0){
+        board += "\n";
+    } else if (i % 2 === 1){
+        board += " ";
+    } else if (i % 2 === 0){
+        board += "#";
+    } else {
+        console.log("Error");
+    }
+}
+
+console.log(board);
+*/
+
+
+
+/*
+// Single for loop with singular size (8)
+let board = "";
+let even = " # # # #\n";
+let odd = "# # # # \n";
+
+for (let i = 0; i < 8; i++){
+    if (i % 2 === 0)
+        board += even;
+    else
+        board += odd;
+}
+
+console.log(board);
+*/
+
+
+// Two separate for loops for any size 
+const args = process.argv.slice(2);
+const size = args[0];
+let board = "";
+let even = "";
+let odd = "";
+
+//construct the "even" and "odd" # row strings
+for (let i = 0; i < size; i++){
+    even += i % 2 === 0 ? " " : "#";
+    odd += i % 2 === 0 ? "#" : " ";
+
+    // Same output with an if statement instead of ternary operators
+    // if (i % 2 === 0) {
+    //     even += " ";
+    //     odd += "#";
+    // } else {
+    //     even += "#";
+    //     odd += " ";
+    // }
+}
+
+//construct the output
+for (let i = 0; i < size; i++){
+    if (i % 2 === 0)
+        board += even;
+    else
+        board += odd;
+    board += "\n";
+}
+
+//output
 console.log(board);
