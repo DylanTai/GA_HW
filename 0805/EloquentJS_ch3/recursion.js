71dde716c1e537948a4407b5bf5f5b38498c18b6
Dylan Tai <dylztai@gmail.com>
2025-08-08 14:06:36 -0400

    Finalize 0805

--- 0805/EloquentJS_ch3/recursion.js
+++ 0805/EloquentJS_ch3/recursion.js
@@ -1,9 +1,9 @@
-//A recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+//A recursive function isEven. The function should accept a single parameter (a positive, whole number) and return a Boolean.
 
 function isEven(num){
-    if (num == 0)
+    if (num === 0)
         return true;
-    if (num == 1)
+    if (num === 1)
         return false;
     // Added a fix to numbers that are not positive
     return isEven(num > 0 ? num - 2 : num + 2);
